1c28e5e088625ff812aa5c61560e864656534b48
myelsoria <myrell.05@gmail.com>
2021-04-07 03:40:41 +0800

    created add todo form

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react'
+import AddForm from './AddForm'
 import Todos from './Todos'
 
 export class TodoContainer extends Component {
   state = {
-    todoList: []
+    todoList: [],
   }
 
-  deleteTodoHandler = id=> {
-    const filteredTodoList = this.state.todoList.filter(todo => todo.id !== id)
-    this.setState({todoList: (filteredTodoList)})
+  addTodoHandler = (todo) => {
+    this.setState({ todoList: [...this.state.todoList, todo] })
+  }
+
+  deleteTodoHandler = (id) => {
+    const filteredTodoList = this.state.todoList.filter(
+      (todo) => todo.id !== id
+    )
+    this.setState({ todoList: filteredTodoList })
   }
 
   render() {
     return (
       <>
+        <AddForm onAddTodo={this.addTodoHandler} />
         <Todos
           todoList={this.state.todoList}
           onDeleteTodo={this.deleteTodoHandler}
